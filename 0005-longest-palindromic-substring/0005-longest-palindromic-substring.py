e660ff9e350aff851cc35b619e52a89ceed6e094
Lim jihyeok <76669119+kim5288403@users.noreply.github.com>
2023-12-14 15:53:39 +0900

    Time: 498 ms (57.79%), Space: 16.4 MB (56.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,20 +3,19 @@ class Solution:
         max_s, max_e = 0, 0
         
         for i in range(len(s)):
-            
             start, end = i, i
-            while 0 <= start and len(s) > end and s[start] == s[end]:
+            
+            while start >=0 and end < len(s) and s[start] == s[end]:
                 if max_e - max_s < end - start:
-                    max_s, max_e = start, end
-                start, end = start - 1, end + 1
-                
-            start, end = i, i + 1
-            while 0 <= start and len(s) > end and s[start] == s[end]:
-                if max_e - max_s < end - start:
-                    max_s, max_e = start, end
+                    max_e, max_s = end, start
                 start, end = start - 1, end + 1
             
-   
-        return s[max_s : max_e + 1]
+            start, end = i, i + 1
+            while start >= 0 and end < len(s) and s[start] == s[end]:
+                if max_e - max_s < end - start:
+                    max_e, max_s = end, start
+                start, end = start - 1, end + 1
+                    
+        return s[max_s: max_e + 1]
 
 
